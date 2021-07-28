8b065b68924ebd791e5c0c41208bd699a6cabd57
ilse moreno <ilsemoreno@Stephanie-Moreno.local>
2021-07-27 23:34:17 -0500

    se agrego css y se hicieron divs de productos

--- main.js
+++ main.js
@@ -1,135 +1,33 @@
-let ingresaNombre = prompt("¡Hola! ¿cual es tu nombre?");
-
-
-class Ropa {
-    constructor(talla, genero, categoria, total) {
-        this.talla = talla;
-        this.genero = genero;
-        this.categoria = categoria;
-        this.total = parseInt(total);
-    }
-}
-const ropa = new Ropa();
-
-const carritoCompra = [];
-
-
-
-menu()
-
-
-function categorias(cat) {
-    if (cat == 1) {
-        return "playera"
-    }
-    if (cat == 2) {
-        return "pantalones"
-    }
-    if (cat == 3) {
-        return "ropa interior"
-    }
-    if (cat == 4) {
-        return "calzado"
-    }
-    if (cat == 5) {
-        return "ropa deportiva"
-    }
+function cosmetico(id, nombre, descripcion, color, precio, imagen) {
+    this.id = parseInt(id);
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.imagen = imagen;
+    this.color = color;
+    this.precio = parseFloat(precio);
 }
 
-function mostrarCarrito() {
-    let totalCarrito = parseInt(prompt("¿Cuantos vas a comprar?"));
-    return totalCarrito;
-}
-
-function menu() {
-    let inicio = parseInt(prompt("¡Hola! " + ingresaNombre + " ¿Que producto deseas comprar? \n1)Playera \n2)Pantalones \n3)Ropa Interior \n4)Calzado \n5)Ropa Deportiva "));
-
-    if (inicio == 1) {
-        let fin = categorias(inicio);
-        let carrito = mostrarCarrito();
-        let cuestionTalla = preguntaTalla();
-        let cuestionGenero = preguntarGenero();
-        preguntar(fin, cuestionGenero, cuestionTalla, carrito);
-    } else if (inicio == 2) {
-        let fin = categorias(inicio);
-        let carrito = mostrarCarrito();
-        let cuestionTalla = preguntaTalla();
-        let cuestionGenero = preguntarGenero();
-        preguntar(fin, cuestionGenero, cuestionTalla, carrito);
-
-    } else if (inicio == 3) {
-        let fin = categorias(inicio);
-        let carrito = mostrarCarrito();
-        let cuestionTalla = preguntaTalla();
-        let cuestionGenero = preguntarGenero();
-        preguntar(fin, cuestionGenero, cuestionTalla, carrito);
-    } else if (inicio == 4) {
-        let fin = categorias(inicio);
-        let carrito = mostrarCarrito();
-        let cuestionTalla = preguntaTalla();
-        let cuestionGenero = preguntarGenero();
-        preguntar(fin, cuestionGenero, cuestionTalla, carrito);
-
-    } else if (inicio == 5) {
-        let fin = categorias(inicio);
-        let carrito = mostrarCarrito();
-        let cuestionTalla = preguntaTalla();
-        let cuestionGenero = preguntarGenero();
-        preguntar(fin, cuestionGenero, cuestionTalla, carrito);
-
-    } else {
-        alert("opccion invalida")
-    }
-
-}
+const cosmeticos = [];
+cosmeticos.push(new cosmetico(1, "Labial Studio Look Mate", "El favorito de todas. Labial líquido Studio Look mate de larga duración, un must-have para combinar todos tus looks. 6ml / 0.17fl.oz.", "LATTE", 109.90, "https://cyzone.tiendabelcorp.com/cdn-cgi/image/width=1200,fit=contain,f=auto/https://belc-bigdata-mdm-images-prd.s3.amazonaws.com/images/FotoProductoFondoBlancoWebRedes/200103920_fotofondoblanco.jpg"));
+cosmeticos.push(new cosmetico(2, "spray fijador maquillaje studio look", "makeup intacto. el spray fijador hará que tu maquillaje dure por más tiempo además de brindarle un acabado mate a tu rostro. 60ml/2.04 fl.oz", "Transparente", 169.90, "https://cyzone.tiendabelcorp.com/cdn-cgi/image/width=1200,fit=contain,f=auto/https://belc-bigdata-mdm-images-prd.s3.amazonaws.com/images/FotoProductoFondoBlancoWebRedes/200102371_fotofondoblanco.jpg"));
+cosmeticos.push(new cosmetico(3, "Delineador Plumón Studio Look Para Ojos", "Delineado perfecto. Delineador Plumón Studio Look Para Ojos de alta precisión, color negro intenso y fácil aplicación. 1ml.", "Negro", 109.90, "https://cyzone.tiendabelcorp.com/cdn-cgi/image/width=1200,fit=contain,f=auto/https://belc-bigdata-mdm-images-prd.s3.amazonaws.com/images/FotoProductoFondoBlancoWebRedes/210095652_fotofondoblanco.jpg"));
+cosmeticos.push(new cosmetico(4, "Labial Graduable Mate Studio Look", "¡Tú decides la intensidad! Labial en barra con acabado mate de textura ultra ligera 2g.", "Blush nude", 109.98, "https://cyzone.tiendabelcorp.com/cdn-cgi/image/width=1200,fit=contain,f=auto/https://belc-bigdata-mdm-images-prd.s3.amazonaws.com/images/FotoProductoFondoBlancoWebRedes/200098797_fotofondoblanco.jpg"));
+cosmeticos.push(new cosmetico(5, "Delineador De Cejas Plumón 2 En 1 Wow Brow Studio Look", "Cejas naturales contigo todo el día. Delineador De Cejas Plumón 2 En 1 Wow Brow Studio Look de larga duración con efecto microblading 0.6 ml.", "Dark Brown", 154.90, "https://cyzone.tiendabelcorp.com/cdn-cgi/image/width=1200,fit=contain,f=auto/https://belc-bigdata-mdm-images-prd.s3.amazonaws.com/images/FotoProductoFondoBlancoWebRedes/210097511_fotofondoblanco.jpg"));
+cosmeticos.push(new cosmetico(6, "Paleta Para Rostro Studio Look", "5 productos, 1 paleta. La Paleta Para Rostro Studio Look 5 en 1 es la más completa para resaltar, definir, broncear, dar color e iluminar tu rostro.", "Calidos", 229.90, "https://cyzone.tiendabelcorp.com/cdn-cgi/image/width=1200,fit=contain,f=auto/https://belc-bigdata-mdm-images-prd.s3.amazonaws.com/images/FotoProductoFondoBlancoWebRedes/200102276_fotofondoblanco.jpg"));
+cosmeticos.push(new cosmetico(7, "Base de Maquillaje Studio Look Mate de Alta Cobertura", "Base de maquillaje con efecto mate de alta cobertura y larga duración. fsp 20 + uva. 30g.", "Espresso", 209.90, "https://cyzone.tiendabelcorp.com/cdn-cgi/image/width=1200,fit=contain,f=auto/https://belc-bigdata-mdm-images-prd.s3.amazonaws.com/images/FotoProductoFondoBlancoWebRedes/200100849_fotofondoblanco.jpg"));
 
 
-function preguntar(categoria, genero, talla, total) {
-    let eleccion = parseInt(prompt("¿Deseas seguir comprando? \n1) Si \n2) No"));
-    if (eleccion == 1) {
-        carritoCompra.push(new Ropa(talla, genero, categoria, total));
-        menu();
-    } else if (eleccion == 2) {
-        carritoCompra.push(new Ropa(talla, genero, categoria, total));
-        let resultado = "";
-        let totalCompra = 0;
-        for (let index = 0; index < carritoCompra.length; index++) {
-            resultado = resultado + carritoCompra[index].total + " de " + carritoCompra[index].categoria + " de genero " + carritoCompra[index].genero + " de talla " + carritoCompra[index].talla + "\n";
-            totalCompra = totalCompra + carritoCompra[index].total;
-        }
-        alert("Gracias por tu compra " + "\n" + resultado + "Total de prendas compradas: " + totalCompra);
-        return;
-    } else {
-        alert("Elige una opcion valida")
-        preguntar(categoria, genero, talla, total);
-
-    }
-}
-
-function preguntaTalla() {
-    let talla = parseInt(prompt("¿Que talla deseas elegir? \n1) Chica \n2) Mediana \n3) Grande"));
-
-    if (talla == 1) {
-        return "chica";
-    } else if (talla == 2) {
-        return "mediana";
-    } else if (talla == 3) {
-        return "grande";
-    } else {
-        alert("Elige una opcion valida");
-        return preguntaTalla();
-    }
-
-}
-
-function preguntarGenero() {
-    let genero = parseInt(prompt("¿Que genero quieres comprar? \n1) Mujer \n2) Hombre"))
-    if (genero == 1) {
-        return "mujer";
-    } else if (genero == 2) {
-        return "hombre";
-    } else {
-        alert("Elige una opcion valida");
-        return preguntarGenero();
-    }
+for (const cosmetico of cosmeticos) {
+    let divCosmetico = document.createElement("div");
+    divCosmetico.classList.add("card");
+    divCosmetico.innerHTML = `<div class="card contenedorInformacion img-fluid row"> 
+    <img src=${cosmetico.imagen} alt="labial" class="bd-placeholder-img card-img-top imagenContenedor bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid ">
+    <div class="card-body ">
+    <h1 class="card-title"> ${cosmetico.nombre} <h1> 
+    <p class="card-text">${cosmetico.descripcion}</p>
+    <p class="card-text">${cosmetico.color}</p>
+        <h2 class="card-title"> Precio $ ${cosmetico.precio} </h2> <button> agregar </button> 
+        </div>
+        </div>`;
+    document.body.appendChild(divCosmetico);
 }
