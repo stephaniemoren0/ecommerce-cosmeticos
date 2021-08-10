3924b2ac0b20cc55e15d2aed16c355a7ca64cbe5
ilse moreno <ilsemoreno@Stephanie-Moreno.local>
2021-08-09 21:41:54 -0500

    se agrego jquerry

--- js/main.js
+++ js/main.js
@@ -2,20 +2,23 @@ var seccionContenedor = document.getElementById("laFuncion")
 
 for (const cosmetico of cosmeticos) {
     seccionContenedor.innerHTML += `
-    <div class="card-group col-4">
-        <div class="card col-md contenedorPrincipal">
-            <img src=${cosmetico.imagen} class="card-img-top trans imagenContenedor bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid" alt="cosmetico">
+    <div>
+        <div class="card">
+    <img src=${cosmetico.imagen} class="card-img-top trans imagenContenedor bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid" alt="cosmetico">
             <div class="card-body">
-                <h1 class="card-title">${cosmetico.nombre}</h1>
-                <p class="card-text">${cosmetico.descripcion}</p>
+            <h5 class="card-title">${cosmetico.nombre}</h5>
+            <p class="card-text">${cosmetico.descripcion}</p>
             <p class="card-text">${cosmetico.color}</p>
-            <h2 class="card-title"> Precio $ ${cosmetico.precio} </h2> <button> agregar </button> 
+            <h2 class="card-title"> Precio $ ${cosmetico.precio} </h2> 
+            <a href="#" class="btn btn-primary">Comprar</a>
             </div>
         </div>
     </div>
     <br>
     `
 }
+
+
 
 const busqueda = document.getElementById("miBusqueda");
 
